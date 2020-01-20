ecce3a44d199c5dfcee379590c694314440a112e
Jarrod Holder <jarrodh@theswitch.us>
2020-01-20 13:48:17 -0600

    removes setInterval logic so that heroku can manage schedule

--- src/index.js
+++ src/index.js
@@ -10,17 +10,6 @@ const appLoop = () => {
                     stk.getMessages()
                 }
 
-let appInterval = setInterval(appLoop, 60000)
-
-setInterval(async () => {
-    const clock = await alpAPI.get('clock');
-    const now = new Date();
-    if(!clock.is_open && now.getHours < 9 || now.getHours > 17)
-    {
-        appInterval.clearInterval()
-    } else if(appInterval == undefined) {
-        appInterval = setInterval(appLoop, 60000)
-    }
-}, (3600000/4))
+setInterval(appLoop, 60000)
 
 
