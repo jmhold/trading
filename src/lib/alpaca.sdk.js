24927ce3db2e5fa06e2b51e0c6c980375af6519a
Jarrod Holder <jarrod.holder@wwt.com>
2020-02-13 09:48:15 -0600

    forgot the new variables

--- src/lib/alpaca.sdk.js
+++ src/lib/alpaca.sdk.js
@@ -1,7 +1,9 @@
 import Alpaca from '@alpacahq/alpaca-trade-api'
 import {
     PAPER_APCA_API_KEY,
-    PAPER_APCA_API_SECRET
+    PAPER_APCA_API_SECRET,
+    APCA_API_KEY,
+    APCA_API_SECRET
 } from './alpaca.vars'
 
 // Alpaca SDK
